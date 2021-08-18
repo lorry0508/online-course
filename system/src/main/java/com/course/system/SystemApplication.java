7dc9d9bd0d7032204f0286026a8fb1d119f3aeb6
LR13204 <lr13204@cnki.net>
2021-08-18 15:01:04 +0800

    1. mybatis使用示例：http://127.0.0.1:9001/system/test 2. 增加三层结构

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -1,23 +1,26 @@
 package com.course.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.system.mapper")
 public class SystemApplication {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+  private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(SystemApplication.class);
-		Environment env = app.run(args).getEnvironment();
-		LOG.info("启动成功！！");
-		LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
-	}
-
+  public static void main(final String[] args) {
+    final SpringApplication app = new SpringApplication(SystemApplication.class);
+    final Environment env = app.run(args).getEnvironment();
+    LOG.info("启动成功！！");
+    LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+  }
 }

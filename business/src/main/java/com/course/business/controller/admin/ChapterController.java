e0cfbbc39151b4428f10f508f44c2c350b812ce4
LR13204 <lr13204@cnki.net>
2021-09-06 17:19:15 +0800

    1. 增加分页组件pagination.vue 2. 大章管理页面使用分页组件，可自定义初始每页5条，最多显示8个按钮

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,7 +1,5 @@
 package com.course.business.controller.admin;
 
-import com.course.server.domain.Chapter;
-import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
 import org.slf4j.Logger;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/chapter")

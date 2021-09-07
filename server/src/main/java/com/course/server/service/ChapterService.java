d8450f8f621d4b2e3503e4ef27a64c48940b0857
LR13204 <lr13204@cnki.net>
2021-09-07 11:06:16 +0800

    1. 前端增加统一日志输出 2. 加上注释

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -23,6 +23,9 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
+    /**
+     * 列表查询
+     */
     public void list(PageDto pageDto) {
         PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
@@ -40,6 +43,9 @@ public class ChapterService {
         pageDto.setList(chapterDtoList);
     }
 
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
     public void save(ChapterDto chapterDto) {
         Chapter chapter = CopyUtil.copy(chapterDto, Chapter.class);
         if (StringUtils.isEmpty(chapterDto.getId())) {
@@ -49,15 +55,24 @@ public class ChapterService {
         }
     }
 
+    /**
+     * 新增
+     */
     public void insert(Chapter chapter) {
         chapter.setId(UuidUtil.getShortUuid());
         chapterMapper.insert(chapter);
     }
 
+    /**
+     * 更新
+     */
     public void update(Chapter chapter) {
         chapterMapper.updateByPrimaryKey(chapter);
     }
 
+    /**
+     * 删除
+     */
     public void delete(String id) {
         chapterMapper.deleteByPrimaryKey(id);
     }

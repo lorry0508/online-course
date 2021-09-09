2eb20f53764896a1da9c0f5b4112a649cabfa259
LR13204 <lr13204@cnki.net>
2021-09-09 09:13:42 +0800

    com.mysql.jdbc.Driver 替换为 com.mysql.cj.jdbc.Driver

--- generator/src/main/java/com/course/generator/util/DbUtil.java
+++ generator/src/main/java/com/course/generator/util/DbUtil.java
@@ -11,7 +11,7 @@ public class DbUtil {
     public static Connection getConnection() {
         Connection conn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/courseimooc";
             String user = "courseimooc";
             String pass = "courseimooc";

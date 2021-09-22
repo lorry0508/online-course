4036d9f9e335819cfc84008018834cdc8a534ee6
LR13204 <lr13204@cnki.net>
2021-09-22 19:38:07 +0800

    1. 课程管理增加自定义排序

---
+++ server/src/main/java/com/course/server/dto/SortDto.java
@@ -0,0 +1,51 @@
+package com.course.server.dto;
+
+public class SortDto {
+    /**
+     * ID
+     */
+    private String id;
+
+    /**
+     * 当前排序
+     */
+    private int oldSort;
+
+    /**
+     * 新排序
+     */
+    private int newSort;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getOldSort() {
+        return oldSort;
+    }
+
+    public void setOldSort(int oldSort) {
+        this.oldSort = oldSort;
+    }
+
+    public int getNewSort() {
+        return newSort;
+    }
+
+    public void setNewSort(int newSort) {
+        this.newSort = newSort;
+    }
+
+    @Override
+    public String toString() {
+        return "SortDto{" +
+                "id='" + id + '\'' +
+                ", oldSort=" + oldSort +
+                ", newSort=" + newSort +
+                '}';
+    }
+}

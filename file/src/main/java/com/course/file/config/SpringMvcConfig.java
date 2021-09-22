a552b9e5ce0e4a8ce7e70ef5bfb8c21c5539d196
LR13204 <lr13204@cnki.net>
2021-09-22 22:39:12 +0800

    1. 文件上传功能开发：文件上传实时显示与保存

--- file/src/main/java/com/course/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/course/file/config/SpringMvcConfig.java
@@ -9,6 +9,6 @@ public class SpringMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/file/imooc/course");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/file/imooc/course/");
     }
 }

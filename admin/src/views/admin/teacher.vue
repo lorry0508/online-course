05ca25ec6f8102a2d48560b1f6314ecdf5b668c9
LR13204 <lr13204@cnki.net>
2021-09-23 22:52:53 +0800

    1. 文件上传功能开发：文件按用途分类保存

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -86,6 +86,7 @@
                   <file v-bind:id="'image-upload'"
                         v-bind:text="'上传头像'"
                         v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                        v-bind:use="FILE_USE.TEACHER.key"
                         v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
@@ -134,6 +135,7 @@
       return {
         teacher: {},
         teachers: [],
+        FILE_USE: FILE_USE
       }
     },
     mounted: function() {
@@ -142,7 +144,6 @@
       _this.list(1);
       // sidebar激活样式方法一
       // this.$parent.activeSidebar("business-teacher-sidebar");
-
     },
     methods: {
       /**

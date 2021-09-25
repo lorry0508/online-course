d4d0c9821da83a67877ff4f4be2708d5b3a3c876
LR13204 <lr13204@cnki.net>
2021-09-25 20:09:47 +0800

    1. 大文件断点续传与极速秒传：分片相关的数据传输

--- server/src/main/java/com/course/server/dto/FileDto.java
+++ server/src/main/java/com/course/server/dto/FileDto.java
@@ -1,6 +1,7 @@
 package com.course.server.dto;
 
 import java.util.Date;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class FileDto {
@@ -38,14 +39,22 @@ public class FileDto {
     /**
      * 创建时间
      */
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createdAt;
 
     /**
      * 修改时间
      */
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updatedAt;
+
+    private Integer shardIndex;
+
+    private Integer shardSize;
+
+    private Integer shardTotal;
+
+    private String key;
 
     public String getId() {
         return id;
@@ -111,23 +120,53 @@ public class FileDto {
         this.updatedAt = updatedAt;
     }
 
+    public Integer getShardIndex() {
+        return shardIndex;
+    }
+
+    public void setShardIndex(Integer shardIndex) {
+        this.shardIndex = shardIndex;
+    }
+
+    public Integer getShardSize() {
+        return shardSize;
+    }
+
+    public void setShardSize(Integer shardSize) {
+        this.shardSize = shardSize;
+    }
+
+    public Integer getShardTotal() {
+        return shardTotal;
+    }
+
+    public void setShardTotal(Integer shardTotal) {
+        this.shardTotal = shardTotal;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", path=").append(path);
-        sb.append(", name=").append(name);
-        sb.append(", suffix=").append(suffix);
-        sb.append(", size=").append(size);
-        sb.append(", use=").append(use);
-        sb.append(", createdAt=").append(createdAt);
-        sb.append(", updatedAt=").append(updatedAt);
-        sb.append("]");
-        return sb.toString();
+        return "FileDto{" +
+                "id='" + id + '\'' +
+                ", path='" + path + '\'' +
+                ", name='" + name + '\'' +
+                ", suffix='" + suffix + '\'' +
+                ", size=" + size +
+                ", use='" + use + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", shardIndex=" + shardIndex +
+                ", shardSize=" + shardSize +
+                ", shardTotal=" + shardTotal +
+                ", key='" + key + '\'' +
+                '}';
     }
-
 }
